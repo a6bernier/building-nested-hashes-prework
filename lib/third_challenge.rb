e9e2ad86fa576c7ddb08753e88fcd5bdbd927643
Andrew Bernier <a6bernier@gmail.com>
2018-09-10 22:15:10 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,8 +12,8 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
-        :age => "51"
+        name: "Lady Capulet"
+        age: "51"
         },
       :heroine => {
         :name => "Juliet"
